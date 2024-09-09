f7119332024a5599e04da9f2a6452e8036f7600b
Karolina Balszan <kbalszan@gmail.com>
2024-09-09 14:36:02 +0100

    Fixed cors issue

--- src/Services/flickrApi.js
+++ src/Services/flickrApi.js
@@ -2,10 +2,11 @@ import axios from "axios"
 
 const FLICKR_API_BASE_URL = "https://www.flickr.com/services/rest/"
 const FLICKR_API_KEY = "a28525dbb5ddd24166a80373ef9d4357"
+const CORS_PROXY = "https://cors-anywhere.herokuapp.com/"
 
 export async function getBirdImage(birdName) {
   try {
-    const response = await axios.get(FLICKR_API_BASE_URL, {
+    const response = await axios.get(`${CORS_PROXY}${FLICKR_API_BASE_URL}`, {
       params: {
         method: "flickr.photos.search",
         api_key: FLICKR_API_KEY,
